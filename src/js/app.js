36a80a698d06907f8d42863fd41b66b3d343353f
Elliot Plant <elliotaplant@gmail.com>
2016-04-28 21:29:30 -0700

    Created body component

--- src/js/app.js
+++ src/js/app.js
@@ -9,36 +9,52 @@ require('aframe-template-component');
 // import Cursor from './components/Cursor';
 // import Sky from './components/Sky';
 
+class Force {
+  constructor(a, b) {
+    this.a = a;
+    this.b = b;
+    this.val = 0;
+  }
+  update() {
+
+  }
+}
+
 class SpaceScene extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      color: 'red'
-    }
+      forces: [],
+      marsPos: [5, 9, -20],
+      earthPos: [0, 0, -10]
+    };
+    // setInterval(() => {
+    //   this.setState({
+    //     marsPos: this.state.marsPos.map(val => val+0.01),
+    //     earthPos: this.state.earthPos.map(val => val-0.01)
+    //   },100)
+    // })
   }
-
-  changeColor = () => {
-    const colors = ['red', 'orange', 'yellow', 'green', 'blue'];
-    this.setState({
-      color: colors[Math.floor(Math.random() * colors.length)],
-    });
-  };
 
   render() {
     return (
       <Scene>
         <a-assets>
           <img id="outer-space" src="../assets/equi-sky.png" />
-          <img id="enemy-sprite" src="../assets/cow_texture.png" />
+          <img id="cow" src="../assets/cow_texture.png" />
+          <img id="earth-img" src="../assets/earth.jpg" />
+          <img id="mars-img" src="../assets/mars.png" />
         </a-assets>
 
-        <Entity layout="type: circle; radius: 5" position="0 0.5 0">
-          {[0,1,2,2,2,2,2,2,2,2,2].map((item, index) =>
-              <a-image key={index} look-at="#player" src="#enemy-sprite" transparent="true" />)
-          }
-          <Animation attribute="rotation" dur="5000" ease="linear" from="0 0 0" to="360 360 360" repeat="indefinite" />
-        </Entity>
-
+        <a-sphere id='earth'
+          src='#earth-img'
+          radius='4'
+          position={this.state.earthPos.join(' ')} />
+        <a-sphere id='mars'
+          src='#mars-img'
+          radius='3'
+          position={this.state.marsPos.join(' ')}/>
+        <Body />
         <a-camera id="player" position="0 1.8 0"></a-camera>
 
         <a-sky src="#outer-space"></a-sky>
@@ -47,34 +63,23 @@ class SpaceScene extends React.Component {
   }
 }
 
-const Sky = () => <a-sky color="skyblue"></a-sky>;
-
-const Lights = () => (
-  <div>
-    <a-light type="spot" color="#333" position="-20 0 0" look-at="a-sphere"></a-light>
-    <a-light type="point" color="#AAA" position="0 5 0"></a-light>
-  </div>
-);
-
-const Sphere = () => {
-  return (
-  <a-sphere color="steelblue" radius="4"
-    position="-5 2 -10"
-  >
-    <a-animation attribute="rotation" begin="click"
-      dur ="5000" repeat="indefinite" to="360 0 0"
-    />
-    <a-event name="mouseenter" scale="1 1.5 1"></a-event>
-    <a-event name="mouseleave" scale="1 1 1"></a-event>
-  </a-sphere>
-)};
-
-
-const Cursor = () => (
-  <a-camera position="0 2 0">
-    <a-cursor color="#2E3A87" />
-  </a-camera>
-);
-
+class Body extends React.Component {
+  constructor(props) {
+    super(props);
+    this.src = props.src || '#cow';
+    this.mass = props.mass || 50;
+    const position = new THREE.Vector3(0, 0, -5)
+    this.state = { position };
+  }
+  render() {
+    return (
+      <a-sphere id='earth'
+        src="#cow"
+        radius={Math.log2(this.mass)}
+        position={this.state.position.toArray().join(' ')}
+      />
+    )
+  }
+}
 
 ReactDOM.render(<SpaceScene/>, document.querySelector('.scene-container'));
