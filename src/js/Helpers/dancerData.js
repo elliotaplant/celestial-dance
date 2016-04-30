037f920a2b3383c088f3738a4354effb68e22829
Elliot Plant <elliotaplant@gmail.com>
2016-04-30 12:29:08 -0700

    Make dummy dancer data and export it

--- src/js/Helpers/dancerData.js
+++ src/js/Helpers/dancerData.js
@@ -1,7 +1,9 @@
 const makeDancer = (mass, x, y, z) => ({ mass, position: [x, y, z] });
-module.export = [
+const dancerData = [
   makeDancer(100, -3, 4, -8),
   makeDancer(200, -2, -1, -12),
   makeDancer(700, 3, 2, -8),
   makeDancer(1200, 1, -2, -7),
 ];
+
+module.exports = dancerData;
