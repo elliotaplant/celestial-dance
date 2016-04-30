bfcc4bf49f438c336e6ea8fafdd445199e2122c6
Elliot Plant <elliotaplant@gmail.com>
2016-04-30 12:33:08 -0700

    Add lots of fun new functions

--- src/js/Helpers/VectorHelpers.js
+++ src/js/Helpers/VectorHelpers.js
@@ -1,3 +1,7 @@
+const GRAVITY = 0.00010;
+
+const massToRadius = mass => Math.log2(mass)/10;
+
 const translate = (object3D, velocity) => {
   object3D.translateX(velocity.x);
   object3D.translateY(velocity.y);
@@ -6,13 +10,36 @@ const translate = (object3D, velocity) => {
 
 const rand = (min, max) => min + Math.random()*(max - min);
 
-const getNetForce = (bodies, origin) => {
-  console.log('bodies', bodies);
-  console.log('origin', origin);
+const vLog = (v, msg) => console.log(JSON.stringify(v.toArray()), msg);
+
+const getAccel = (origin, body) => {
+  const rMag2 = body.position.distanceToSquared(origin);
+  let rNorm = new THREE.Vector3();
+  rNorm.subVectors(origin, body.position).normalize();
+  return rNorm.multiplyScalar(GRAVITY * body.mass / rMag2);
+};
+
+const getNetAccel = (origin, bodies) => {
+  let netAccel = new THREE.Vector3();
+  for (var i = 0; i < bodies.length; i++) {
+    netAccel.add(getAccel(origin, bodies[i]))
+  }
+  return netAccel;
+};
+
+const map = (array, callback) => {
+  const result = [];
+  for (var i = 0; i < array.length; i++) {
+    result.push(callback(array[i], i, array));
+  }
+  return result;
 }
 
 export {
+  massToRadius,
   translate,
   rand,
-  getNetForce,
+  getNetAccel,
+  map,
+  vLog,
 }
