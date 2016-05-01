8762dd0d4973d9f0f53e076d14e4d47fb2d2218f
Elliot Plant <elliotaplant@gmail.com>
2016-04-30 18:37:03 -0700

    Functioning gravity!!!

--- src/js/components/Dancer.js
+++ src/js/components/Dancer.js
@@ -1,27 +1,35 @@
 import 'aframe';
 import {Animation, Entity} from 'aframe-react';
 import React from 'react';
-import { translate, rand, getNetForce, map } from '../Helpers/VectorHelpers';
+import { translate, rand, getNetAccel, map, vLog,
+  massToRadius, filterClose, vectorToString } from '../Helpers/VectorHelpers';
 
 const backwards = new THREE.Vector3(0, 0, 0.01);
+const TIME_STEP = 1;
 
-AFRAME.registerComponent('update', {
+AFRAME.registerComponent('step', {
   schema: {
-    velocity: { default: new THREE.Vector3(0, 0, -2) },
-    speed: { default: -0.5 }
+    chi: { default: new THREE.Vector3() },
   },
 
-  tick: function () {
+  tick: function (t) {
+    let position = this.el.object3D.position;
+    const radius = +this.el.attributes.radius.value;
+    let velocity = new THREE.Vector3().fromArray(
+      this.el.attributes.velocity.value.split(' ').map(i => +i)
+    );
     let allDancers = this.el.sceneEl.querySelectorAll('.dancer');
     allDancers = map(allDancers,
       dancer => ({
         mass: +dancer.attributes.mass.value,
         position: dancer.object3D.position,
       }));
-    const netForce = getNetForce(allDancers, this.el.object3D.position);
-    // console.log('first z position:', Math.floor(allDancers[0].object3D.position.z));
-    this.data.velocity.add(backwards);
-    translate(this.el.object3D, this.data.velocity);
+    const allOtherDancers = filterClose(allDancers, position, 2*radius);
+    const netAccel = getNetAccel(position, allOtherDancers);
+    velocity = velocity.add(netAccel.multiplyScalar(t)); // addScaledVector doesn't work. lol
+    translate(this.el.object3D, velocity);
+    const velocityString = velocity.toArray().join(' ')
+    this.el.setAttribute('velocity', vectorToString(velocity));
   }
 });
 
@@ -30,11 +38,13 @@ class Dancer extends React.Component {
     super(props);
     this.mass = props.mass || rand(2, 1000);
   }
+
   render() {
     return (
-      <a-sphere src="../assets/earth.jpg" radius={Math.log2(this.mass)/10}
-        update class='dancer' mass={this.mass}
-        position={[rand(-10, 10), rand(-10, 10), rand(-30, -10)]}
+      <a-sphere src="../assets/earth.jpg" radius={massToRadius(this.mass)}
+        step class='dancer' mass={this.mass}
+        velocity="0 0 0"
+        position={this.props.position.join(' ')}
       />
     );
   }
