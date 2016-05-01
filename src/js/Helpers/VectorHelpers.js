46e6d7ec8714df94acca215273fe95c006ce02f7
Elliot Plant <elliotaplant@gmail.com>
2016-04-30 19:46:43 -0700

    Remove accelleration logic from vectorhelpers

--- src/js/Helpers/VectorHelpers.js
+++ src/js/Helpers/VectorHelpers.js
@@ -12,23 +12,6 @@ const rand = (min, max) => min + Math.random()*(max - min);
 
 const vLog = (v, msg) => console.log(msg, JSON.stringify(v.toArray()));
 
-const getAccel = (origin, body) => {
-  const rMag2 = body.position.distanceToSquared(origin);
-  let rNorm = new THREE.Vector3();
-  rNorm.subVectors(body.position, origin).normalize();
-  const result = rNorm.multiplyScalar(GRAVITY * body.mass / rMag2);
-  return result;
-};
-
-const getNetAccel = (origin, bodies) => {
-  let netAccel = new THREE.Vector3();
-  for (var i = 0; i < bodies.length; i++) {
-    netAccel.add(getAccel(origin, bodies[i]))
-  }
-  // vLog(netAccel, 'my net accel')
-  return netAccel;
-};
-
 const filterClose = (dancers, position, radius) => (
   dancers.filter(dancer => dancer.position.distanceTo(position) > radius)
 );
