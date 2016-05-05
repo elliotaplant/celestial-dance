bf8116991c1952a98717c09df9d7b3ba30dd8862
Elliot Plant <elliotaplant@gmail.com>
2016-05-05 14:36:59 -0700

    Remove skeleton code

--- src/js/Helpers/dancerData.js
+++ src/js/Helpers/dancerData.js
@@ -2,16 +2,6 @@ import { randColor } from './Helpers';
 import { ORBITAL_SPEED } from './Constants';
 import { makeDancer, makeRandDancer, makeOrbitalDancer } from './MakeDancers';
 
-
-// Standard orbitals
-// const dancerData = [
-//   makeDancer(100000, [0, 2, -40], [0, 0, 0], '#FFF'),
-//   makeDancer(1000, [20, 2, -40], [0, ORBITAL_SPEED, 0], randColor()),
-//   makeDancer(1000, [0, 22, -40], [0, 0, ORBITAL_SPEED], randColor()),
-//   makeDancer(1000, [-20, 2, -40], [0, -ORBITAL_SPEED, 0], randColor()),
-//   makeDancer(1000, [0, -22, -40], [0, 0, -ORBITAL_SPEED], randColor()),
-// ];
-
 // Random orbitals
 const sun = makeDancer(10000, [0, 0, -50], [0, 0, 0], '#FDB813');
 const dancerData = [
