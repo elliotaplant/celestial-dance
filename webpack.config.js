50a333042196ddd2a8cc8d1d6a78ea258ea2527d
Elliot Plant <elliotaplant@gmail.com>
2016-05-11 18:57:53 -0700

    Update webpack for docker

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,28 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+ template: __dirname + '/src/index.html',
+ filename: 'index.html',
+ inject: 'body'
+});
 var path = require('path');
-var webpack = require('webpack');
-require('babel-polyfill');
-
-var IS_PRODUCTION = process.env.NODE_ENV === 'production';
-
-var ENTRY_POINTS = [
-  './src/js/app'
-];
-
-var JS_LOADERS = [
-  'babel?cacheDirectory&presets[]=react,presets[]=es2015,presets[]=stage-0'
-];
-
-var PLUGINS = [];
-if (IS_PRODUCTION) {
-  // Uglify in production.
-  PLUGINS.push(
-    new webpack.optimize.UglifyJsPlugin({
-      mangle: {
-          except: ['$super', '$', 'exports', 'require']
-      },
-      sourcemap: false
-    })
-  );
-}
 
 module.exports = {
-  entry: ENTRY_POINTS,
-  output: {
-    // Bundle will be served at /bundle.js locally.
-    filename: 'bundle.js',
-    // Bundle will be built at ./src/media/js.
-    path: './build',
-    publicPath: '/',
-  },
-  module: {
-    loaders: [
-      {
-        // JS.
-        exclude: /(node_modules|bower_components)/,
-        loaders: JS_LOADERS,
-        test: /\.js$/,
-      },
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader'
-      },
-      {
-        test: /\.json$/,
-        loader: 'json-loader'
-      }
-    ],
-  },
-  plugins: PLUGINS,
-  resolve: {
-    extensions: ['', '.js', '.json'],
-    fallback: path.join(__dirname, 'node_modules'),
-    modulesDirectories: [
-      'src/js',
-      'node_modules',
-    ]
-  },
-  resolveLoader: {
-    fallback: [path.join(__dirname, 'node_modules')]
-  }
-};
+ entry: [
+   './src/app.js'
+ ],
+ output: {
+   filename: 'index_bundle.js',
+   path: __dirname + '/build'
+ },
+ cache: true,
+ debug: true,
+ devtool: 'source-map',
+ module: {
+   loaders: [
+     {
+       test: /\.js$/, include: __dirname + '/src', loader: 'babel-loader'
+     }
+   ]
+ },
+ plugins: [HTMLWebpackPluginConfig]
+}
