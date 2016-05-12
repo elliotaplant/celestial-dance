29687b20d22c74f95e4970e3900c5e8eba05e0b1
Elliot Plant <elliotaplant@gmail.com>
2016-05-11 18:53:39 -0700

    Add server file

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+var PORT = process.env.PORT || 5555;
+var ENV = process.env.NODE_ENV || 'development';
+
+app.listen(PORT);
+console.log('Node environment:', ENV)
+console.log('Server listening on port', PORT);
+app.use(express.static(__dirname + '/build'));
