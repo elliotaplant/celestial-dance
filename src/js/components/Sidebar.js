3d7cea96c34a0249eb0c6e757829bda010d345da
Elliot Plant <elliotaplant@gmail.com>
2016-05-19 11:25:58 -0700

    Format sidebar

--- src/js/components/Sidebar.js
+++ src/js/components/Sidebar.js
@@ -2,23 +2,38 @@ import React from 'react';
 
 const Sidebar = () => (
   <div>
-    <input type="checkbox" checked id="drawer-toggle" name="drawer-toggle"/>
+    <input type="checkbox" defaultChecked id="drawer-toggle" name="drawer-toggle"/>
     <label htmlFor="drawer-toggle" id="drawer-toggle-label"></label>
     <div id="drawer">
       <h1>Controls</h1>
-      <h3>Desktop</h3>
-      <p>Fly around with WASD keys</p>
-      <p>Use the mouse to look around</p>
-      <h3>Mobile</h3>
-      <p>Use your phone as a window into space!</p>
-      <p>Press the VR button in the bottom right to enter VR mode with
-        <a href="http://tinyurl.com/jegsfu9"> google cardboard</a></p>
-      <br/>
-      <p>Made by Elliot Plant</p>
-      <p><a href="https://elliotplant.wordpress.com/">Blog</a><a href="http://www.elliotplant.com/">Website</a><a href="https://github.com/elliotaplant">Github</a></p>
-      <p>Currently a work in Progress</p>
-      <p><a href="https://github.com/elliotaplant/celestial-dance">Project Github</a></p>
-      <p><a href="aframe.io">Framework by A-Frame</a></p>
+      <div>
+        <h3>Desktop</h3>
+        <p>
+          Fly around with WASD keys
+        </p>
+        <p>
+          Use the mouse to look around
+        </p>
+      </div>
+      <div>
+        <h3>Mobile </h3>
+        <p>Use your phone as a window into space!</p>
+        <p>
+          Press the VR button in the bottom right to enter VR mode with
+          <a href="http://tinyurl.com/jegsfu9"> google cardboard </a>
+        </p>
+      </div>
+      <div>
+        <h3>Made by Elliot Plant</h3>
+        <p><a href="https://elliotplant.wordpress.com/">Blog</a></p>
+        <p><a href="http://www.elliotplant.com/">Website</a></p>
+        <p><a href="https://github.com/elliotaplant">Github</a></p>
+      </div>
+      <div>
+        <h3>Currently a work in Progress</h3>
+        <p><a href="https://github.com/elliotaplant/celestial-dance">Project Github</a></p>
+        <p><a href="aframe.io">Framework by A-Frame</a></p>
+      </div>
     </div>
   </div>
 );
