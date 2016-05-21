915a13eb9a575a26a93725a08d3ce6d2d34cef9a
Elliot Plant <elliotaplant@gmail.com>
2016-05-20 18:54:07 -0700

    Hide burger menu in VR mode

--- src/js/index.js
+++ src/js/index.js
@@ -11,14 +11,28 @@ import Sidebar from './components/Sidebar';
 ReactDOM.render(<Sidebar/>, document.querySelector('.sidebar-container'));
 ReactDOM.render(<SpaceScene/>, document.querySelector('.scene-container'));
 
+const scene = document.getElementsByTagName('a-scene')[0];
+const xButton = document.getElementById('close-sidebar');
+const hamburgerIcon = document.getElementById('drawer-toggle-label');
+
 function hideDrawer() {
   document.getElementById('drawer-toggle').checked = false;
 }
 
-document.getElementsByTagName('a-scene')[0].onmousedown = hideDrawer;
+function hideBurger() {
+  hamburgerIcon.style.visibility = 'hidden';
+}
 
-const closeSidebar = document.getElementById('close-sidebar')
+function showBurger() {
+  hamburgerIcon.style.visibility = 'visible';
+}
 
-closeSidebar.onmousedown = hideDrawer;
-closeSidebar.onmouseover = () => { closeSidebar.src = 'assets/x-icon-white.png' };
-closeSidebar.onmouseout = () => { closeSidebar.src = 'assets/x-icon-grey.png' };
+scene.onmousedown = hideDrawer;
+
+scene.addEventListener('enter-vr', hideBurger, false);
+scene.addEventListener('exit-vr', showBurger, false);
+
+
+xButton.onmousedown = hideDrawer;
+xButton.onmouseover = () => { xButton.src = 'assets/x-icon-white.png' };
+xButton.onmouseout = () => { xButton.src = 'assets/x-icon-grey.png' };
