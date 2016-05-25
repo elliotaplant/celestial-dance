5e23b7c941325f6eee5a714fdcb9c80e0863f2a5
Elliot Plant <elliotaplant@gmail.com>
2016-05-25 13:48:35 -0700

    Clarify pairing directions

--- src/js/components/Sidebar.js
+++ src/js/components/Sidebar.js
@@ -7,21 +7,19 @@ const Sidebar = () => (
     <div id="drawer">
       <img id='close-sidebar' src='assets/x-icon-grey.png'/>
       <h1>Controls</h1>
-      <div id="desktop">
+      <div className="desktop">
         <p>Fly around with WASD or arrow keys</p>
         <p>Click and drag the mouse to look around</p>
         <p>Try using a mobile device for Virtual Reality experience</p>
         <p>Use your keyboard to control your phone at<a href="https://proxy-controls.donmccurdy.com/#/connect#%2Fconnect"> pair.elliotplant.com</a></p>
       </div>
-      <div id="mobile">
-        <p>For the best experience, add this page to your homescreen (<a href="http://www.ianswerguy.com/add-website-icon-to-home-screen/">iPhone</a>, <a href="http://www.howtogeek.com/196087/how-to-add-websites-to-the-home-screen-on-any-smartphone-or-tablet/">Android</a>)</p>
-        <p>Press the VR button in the bottom right to enter VR mode with
-          <a href="http://tinyurl.com/jegsfu9"> google cardboard </a>
-        </p>
-        <p>To fly around, enter your pair-code on a computer at <a href="https://proxy-controls.donmccurdy.com/#/connect#%2Fconnect"> pair.elliotplant.com</a></p>
+      <div className="mobile">
+        <p>To enter full-screen mode, add this page to your homescreen (<a href="http://www.ianswerguy.com/add-website-icon-to-home-screen/">iPhone</a>, <a href="http://www.howtogeek.com/196087/how-to-add-websites-to-the-home-screen-on-any-smartphone-or-tablet/">Android</a>)</p>
+        <p>Press the VR button in the bottom right to enter VR mode with<a href="http://tinyurl.com/jegsfu9"> google cardboard </a></p>
+        <p>To fly around using the arrow keys on your keyboard as a controller, enter your Pair Code at at pair.elliotplant.com on a computer connected to the same wifi</p>
         <p id="pair-code">Pair Code: </p>
       </div>
-      <footer>
+      <footer className="desktop">
         <div>
           <h3>Made by Elliot Plant</h3>
           <p><a href="https://elliotplant.wordpress.com/">Blog</a></p>
@@ -32,6 +30,7 @@ const Sidebar = () => (
           <h3>Currently a work in Progress</h3>
           <p><a href="https://github.com/elliotaplant/celestial-dance">Project Github</a></p>
           <p><a href="aframe.io">Framework by A-Frame</a></p>
+          <p>Controls by <a href="https://www.donmccurdy.com/">Don McCurdy</a></p>
         </div>
       </footer>
     </div>
