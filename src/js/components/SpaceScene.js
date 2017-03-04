55560e4e9495fb2e38f2a6128463e906ff4c69b2
Elliot Plant <elliotaplant@gmail.com>
2017-03-04 15:23:33 -0800

    Try to update some stuff

--- src/js/components/SpaceScene.js
+++ src/js/components/SpaceScene.js
@@ -10,7 +10,7 @@ AFRAME.registerComponent('proxy-controls', ProxyControls);
 const SpaceScene = () => (
   <a-scene proxy-controls="debug: true">
     <a-assets>
-      <img id="outer-space" src='assets/spaceSky.jpg' />
+      <img id="outer-space" src='./images/spaceSky.jpg' />
       <sound id="ping" src='assets/ping.wav' />
     </a-assets>
     {dancerData.map((dancer, index) => (
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,31 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
- template: __dirname + '/src/index.html',
- filename: 'index.html',
- inject: 'body'
+  template: __dirname + '/src/index.html',
+  filename: 'index.html',
+  inject: 'body'
 });
 var path = require('path');
 
 module.exports = {
- entry: [
-   './src/js/index.js'
- ],
- output: {
-   filename: 'index_bundle.js',
-   path: __dirname + '/build'
- },
- cache: true,
- debug: true,
- devtool: 'source-map',
- module: {
-   loaders: [
-     {test: /\.js$/, include: __dirname + '/src/js', loader: 'babel-loader'},
-     {
-   test: /\.(png|jpg|gif)$/,
-   loader: "file-loader?name=img/img-[hash:6].[ext]"
- }
-   ]
- },
- plugins: [HTMLWebpackPluginConfig]
+  entry: [
+    './src/js/index.js'
+  ],
+  output: {
+    filename: 'index_bundle.js',
+    path: __dirname + '/build'
+  },
+  cache: true,
+  debug: true,
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {test: /\.js$/, include: __dirname + '/src/js', loader: 'babel-loader'},
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        include : path.join(__dirname, 'images'),
+        loader  : 'url-loader?limit=30000&name=images/[name].[ext]'
+      },
+    ]
+  },
+  plugins: [HTMLWebpackPluginConfig]
 }
